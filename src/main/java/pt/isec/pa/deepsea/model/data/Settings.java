1435a23323519e510bce72cdb8c3cbada6da94ef
Vera2024140140 <a2024140140@isec.pt>
2026-04-03 22:53:50 +0100

    alteração de classe settings para interface

--- src/main/java/pt/isec/pa/deepsea/model/data/Settings.java
+++ src/main/java/pt/isec/pa/deepsea/model/data/Settings.java
@@ -1,64 +1,63 @@
 package pt.isec.pa.deepsea.model.data;
 
-public class Settings {
-    public static final boolean MODO_DEFESA = false;
+public interface Settings {
+     boolean MODO_DEFESA = false;
 
     //Dimensao das grelhas
-    public static final int LINHAS_SUPERFICIE = 30;
-    public static final int COLUNAS_SUPERFICIE = 15;
-    public static final int LINHAS_FOSSO = 20;
-    public static final int COLUNAS_FOSSO = 10;
-    public static final int LINHAS_FUNDO = 10;
-    public static final int COLUNAS_FUNDO = 10;
+    int LINHAS_SUPERFICIE = 30;
+    int COLUNAS_SUPERFICIE = 15;
+    int LINHAS_FOSSO = 20;
+    int COLUNAS_FOSSO = 10;
+    int LINHAS_FUNDO = 10;
+    int COLUNAS_FUNDO = 10;
 
     //quantidades e limintes
-    public static final int ID_OBSTACULO_INICIAL = 1001;
-    public static final int ID_ARTEFACTOS_INICIAL = 1;
-    public static final int NUM_DRONES_INICIAIS = 3;
-    public static final int NUM_ARTEFACTOS = 16;
-    public static final int MINERIO_QTD_MIN = 1;
-    public static final int MINERIO_QTD_MAX = 5;
-    public static final int MINERIO_ZONA_MIN = 1;
-    public static final int MINERIO_ZONA_MAX = 1;
-    public static final int OBSTACULOS_FOSSO_MAX = 10;
-    public static final int OBSTACULOS_FOSSO_MIN = 1;
-    public static final int MONSTROS_FUNDO_MIN = 1;
-    public static final int MONSTROS_FUNDO_MAX = 7;
+    int ID_OBSTACULO_INICIAL = 1001;
+    int ID_ARTEFACTOS_INICIAL = 1;
+    int NUM_DRONES_INICIAIS = 3;
+    int NUM_ARTEFACTOS = 16;
+    int MINERIO_QTD_MIN = 1;
+    int MINERIO_QTD_MAX = 5;
+    int MINERIO_ZONA_MIN = 1;
+    int MINERIO_ZONA_MAX = 1;
+    int OBSTACULOS_FOSSO_MAX = 10;
+    int OBSTACULOS_FOSSO_MIN = 1;
+    int MONSTROS_FUNDO_MIN = 1;
+    int MONSTROS_FUNDO_MAX = 7;
 
     //combustivel e integridade casco
-    public static final double NAVIO_COMBUSTIVEL_INICIAL = 1000;
-    public static final double NAVIO_COMBUSTIVEL_MAX = 1000;
-    public static final double COMBUSTIVEL_REPARACAO = 20;
-    public static final double COMBUSTIVEL_MOV_NAVIO = 1;
+    double NAVIO_COMBUSTIVEL_INICIAL = 1000;
+    double NAVIO_COMBUSTIVEL_MAX = 1000;
+    double COMBUSTIVEL_REPARACAO = 20;
+    double COMBUSTIVEL_MOV_NAVIO = 1;
 
-    public static final double DRONE_COMBUSTIVEL_MAX = 200;
-    public static final double DRONE_CONSUMO_MOV = 1;
-    public static final double DRONE_CONSUMO_CORRENTE = 2;
-    public static final int DRONE_INTEGRIDADE_MAX = 100;
+    double DRONE_COMBUSTIVEL_MAX = 200;
+    double DRONE_CONSUMO_MOV = 1;
+    double DRONE_CONSUMO_CORRENTE = 2;
+    int DRONE_INTEGRIDADE_MAX = 100;
 
     //mini jogo
-    public static final int PUZZLE_MAX_MOVIMENTOS = 50;
-    public static final int PUZZLE_GRELHA = 4;
+    int PUZZLE_MAX_MOVIMENTOS = 50;
+    int PUZZLE_GRELHA = 4;
 
     //percentagens
-    public static final double ROCHAS_PERCENTAGEM_MAX = 0.50;
-    public static final double CONSUMO_EXTRA_MINERIO = 0.01;
-    public static final double DANO_MONSTRO_PERCENTAGEM = 0.05;
+    double ROCHAS_PERCENTAGEM_MAX = 0.50;
+    double CONSUMO_EXTRA_MINERIO = 0.01;
+    double DANO_MONSTRO_PERCENTAGEM = 0.05;
 
     //custo e melhoramentos oficina
-    public static final int CUSTO_MINERIOS_MELHORAR_TANQUE = 5;
-    public static final int CUSTO_MINERIOS_MELHORAR_BLINDAGEM = 5;
-    public static final int INCREMENTO_TANQUE_COMBUSTIVEL = 50;
-    public static final int INCREMENTO_BLINDAGEM_MAXIMA = 20;
+    int CUSTO_MINERIOS_MELHORAR_TANQUE = 5;
+    int CUSTO_MINERIOS_MELHORAR_BLINDAGEM = 5;
+    int INCREMENTO_TANQUE_COMBUSTIVEL = 50;
+    int INCREMENTO_BLINDAGEM_MAXIMA = 20;
 
     //modo defesa
-    public static final int DEFESA_NUM_ARTEFACTOS_LADO = NUM_ARTEFACTOS/2;
-    public static final int DEFESA_NUM_ROCHAS_LADO = 1;
-    public static final int DEFESA_MONSTROS = 1;
-    public static final int DEFESA_MINERIOS = 2;
-    public static final int DEFESA_MINERIO_QTD_MAX = 10;
+    int DEFESA_NUM_ARTEFACTOS_LADO = NUM_ARTEFACTOS/2;
+    int DEFESA_NUM_ROCHAS_LADO = 1;
+    int DEFESA_MONSTROS = 1;
+    int DEFESA_MINERIOS = 2;
+    int DEFESA_MINERIO_QTD_MAX = 10;
 
-    private Settings(){};
 
 
 
